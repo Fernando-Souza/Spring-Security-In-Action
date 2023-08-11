bcf6c12dcb2d0ca669ac9b1f929b22f6916830b5
fernando <fernandoantonio.souza@outlook.com>
2023-08-11 18:54:20 -0300

    Início do projeto

---
+++ src/main/java/com/fernando/springsecurityparent/SpringSecurityParentApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.springsecurityparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/springsecurityparent/SpringSecurityParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.springsecurityparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
