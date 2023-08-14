f24d1426273da637417faa66c82598e79be5bc30
fernando <fernandoantonio.souza@outlook.com>
2023-08-14 16:37:07 -0300

    ch03-exec-1

--- ch03/src/main/java/com/estudo/ch03/Ch03Application.java
+++ ch03/src/main/java/com/estudo/ch03/Ch03Application.java
@@ -2,9 +2,24 @@ package com.estudo.ch03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
 
 @SpringBootApplication
 public class Ch03Application {
+
+
+ public void m() {
+        UserDetails u = User.withUsername("bill")
+                .password("12345")
+                .authorities("read", "write")
+                .accountExpired(false)
+                .disabled(true)
+                .build();
+
+
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ch03Application.class, args);
