6c841bf06143b70fa0657a776745f761bebe51da
fernando <fernandoantonio.souza@outlook.com>
2023-08-14 18:38:13 -0300

    Create an H2 datasource

---
+++ ch03/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into USUARIO (username, senha, ativo) values ('joao', '12345', 1);
+insert into USUARIO (username, senha, ativo) values ('maria', '54321', 0);
+insert into USUARIO (username, senha, ativo) values ('pedro', '13245', 1);
+
+
+insert into AUTORIDADE (username, authority) values ('joao', 'write');
+insert into AUTORIDADE (username, authority) values ('maria','read');
+insert into AUTORIDADE (username, authority) values ('pedro','write');
