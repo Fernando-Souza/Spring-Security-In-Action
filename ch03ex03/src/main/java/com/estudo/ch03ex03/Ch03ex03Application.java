26175206b497d60a40b7ad90808ccf426954f09e
fernando <fernandoantonio.souza@outlook.com>
2023-08-15 15:25:34 -0300

    LDAPUserDetailsManager

---
+++ ch03ex03/src/main/java/com/estudo/ch03ex03/Ch03ex03Application.java
@@ -0,0 +1,13 @@
+package com.estudo.ch03ex03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch03ex03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch03ex03Application.class, args);
+	}
+
+}
